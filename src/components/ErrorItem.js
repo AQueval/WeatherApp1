8e5cecce9e5de702b4dda094a1790c6039162265
AQueval <87530892+AQueval@users.noreply.github.com>
2023-07-24 15:49:33 +0200

    Create error screen
    Create screen and add its display condition in App.js.

---
+++ src/components/ErrorItem.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Dimensions, SafeAreaView, StyleSheet, Text, View } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+import mainStyles from "../styles/MainStyle";
+
+const ErrorItem = () => {
+  const { container, errorMessage } = styles;
+
+  return (
+    <View style={container}>
+      <MaterialIcons name="error-outline" size={70} color="#FDF6EF" />
+      <Text style={errorMessage}>Sorry, something went wrong</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#DF1600",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    width: Dimensions.get("window").width,
+  },
+  errorMessage: {
+    color: "#FDF6EF",
+    fontSize: 30,
+    paddingHorizontal: 10,
+    textAlign: "center",
+  },
+});
+
+export default ErrorItem;
